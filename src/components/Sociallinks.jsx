6516c7225e5b34772708a3ecdd8f41c541ebd8a0
nxhettry <nishan.gtsn@gmail.com>
2024-04-10 18:09:18 +0545

    small bug fix

--- src/components/Sociallinks.jsx
+++ src/components/Sociallinks.jsx
@@ -12,14 +12,14 @@ const SocialFollow = () => {
   return (
     <div className="bg-inherit p-6 px-12 flex gap-4">
       <a href="https://www.facebook.com/nissan.gautam.507"
-        className="facebook social">
+        className="facebook social" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faFacebook} size="1x" />
       </a>
-      <a href="https://twitter.com/nishan_gtsn" className="twitter social">
+      <a href="https://twitter.com/nishan_gtsn" className="twitter social" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faTwitter} size="1x" />
       </a>
       <a href="https://www.instagram.com/nis_san_ey/"
-        className="instagram social">
+        className="instagram social" target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={faInstagram} size="1x" />
       </a>
     </div>
